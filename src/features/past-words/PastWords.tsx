130963c75708bd24985ac374d6d903b1e732526f
Andrew Griffith <agriffi10@gmail.com>
2025-04-18 21:19:09 -0400

    most functionality done

--- src/features/past-words/PastWords.tsx
+++ src/features/past-words/PastWords.tsx
@@ -2,17 +2,27 @@ import UserActionButton from "../../components/buttons/UserActionButton";
 import { PastWordsProps } from "../../typing/components/ComponentProps";
 
 export default function PastWords({ wordsList, setShowFinishedWords }: PastWordsProps) {
+  const filteredList = wordsList.filter((x) => x.isSolved);
+
+  const solvedText = (word: string, guesses: string[]) => {
+    let text = "";
+    if (guesses.includes(word)) {
+      text += "Solved in ";
+    } else {
+      text += "Unsolved, ";
+    }
+    text += `${guesses.length} guess${guesses.length == 1 ? "" : "es"}`;
+    return `${word.toUpperCase()} - ${text}`;
+  };
   return (
     <div className="text-left text-white">
       <h2 className="mb-3 text-3xl">Finished Words</h2>
-      <ul>
-        {wordsList
-          .filter((x) => x.isSolved)
-          .map((x) => (
-            <li key={x.word}>
-              {x.word.toUpperCase()} - {x.guesses.includes(x.word) ? "Solved" : "Unsolved"}
-            </li>
-          ))}
+      {filteredList.length == 0 && <p>No words completed yet!</p>}
+
+      <ul className="sm:columns-2">
+        {filteredList.map((x) => (
+          <li key={x.word}>{solvedText(x.word, x.guesses)}</li>
+        ))}
       </ul>
       <div className="mx-auto mt-5 flex w-full max-w-[200px] justify-center">
         <UserActionButton callback={setShowFinishedWords}>Close Finished Words</UserActionButton>
--- src/features/previous/previous-board/PreviousBoard.tsx
+++ src/features/previous/previous-board/PreviousBoard.tsx
@@ -5,6 +5,7 @@ export default function PreviousBoard({ wordList }: PreviousBoardProps) {
   const filteredList = wordList.filter((x) => x.isSolved);
   return (
     <div className="flex flex-wrap items-center justify-center gap-2">
+      {filteredList.length == 0 && <p>No words completed yet!</p>}
       {filteredList.map((wordObject, index) => (
         <PreviousWord
           key={index}
