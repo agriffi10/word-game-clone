185ebb64e04e4feed5828f8ca83097f28e68aa0d
Andrew Griffith <agriffi10@gmail.com>
2025-07-16 09:04:35 -0400

    updating env var name

--- src/api/client/SupabaseClient.ts
+++ src/api/client/SupabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const dbUrl = import.meta.env.VITE_SUPABASE_URL;
+const dbUrl = import.meta.env.VITE_SUPABASE_DATABASE_URL;
 
 const supabase = createClient(dbUrl, import.meta.env.VITE_SUPABASE_ANON_KEY);
 
